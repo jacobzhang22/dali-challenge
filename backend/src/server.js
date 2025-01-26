5b62538646c4424e694783cf87c93a1b88060b07
Jacob Zhang <jaco8123@gmail.com>
2025-01-26 03:29:45 -0500

    Fix auth.js import path for Heroku

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { authorize } = require("../src/auth"); // Import authorize function
+const { authorize } = require("./Auth");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
